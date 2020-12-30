ae1e34d21c7595663f34ea188cffe271a414c249
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-30 12:13:29 +0530

    tangorpro: The full Google Pixel experience!
    * Set the correct features to make apps such as
      Google Camera work.
    
    Thanks fuzzywave40!
    
    Change-Id: I9f04238928599d9e319584b178986c34c6a5b097

--- device-calyx.mk
+++ device-calyx.mk
@@ -1,1 +1,5 @@
 DEVICE_PACKAGE_OVERLAYS += device/google/tangorpro/overlay-calyx
+
+# For Google Camera
+PRODUCT_COPY_FILES += \
+    device/google/tangorpro/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
