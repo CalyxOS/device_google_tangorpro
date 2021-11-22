dbae4307f9d5956c11313b5342654cf139b7f323
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-22 21:22:09 +0530

    Add initial Calyx product makefiles
    Change-Id: I93e489c0e5fb7f8282b25a2b59c8e2e64a4b86b7

---
+++ calyx_tangorpro.mk
@@ -0,0 +1,20 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common_tablet_wifionly.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/gs201/calyx_common.mk)
+$(call inherit-product, device/google/tangorpro/device-calyx.mk)
+$(call inherit-product, device/google/tangorpro/aosp_tangorpro.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_tangorpro
+PRODUCT_MODEL := Pixel Tablet
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=tangorpro \
+    PRIVATE_BUILD_DESC="tangorpro-user 13 TQ3A.230605.009.A1 10100517 release-keys"
+
+BUILD_FINGERPRINT := google/tangorpro/tangorpro:13/TQ3A.230605.009.A1/10100517:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
