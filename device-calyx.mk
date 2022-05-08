29dcc400aadf582bf9d82cc5f141287485260b8f
Michael Bestas <mkbestas@gmail.com>
2022-05-08 03:45:18 +0300

    Add privapp permissions for HbmSVManager
    Change-Id: Ieab465f478424d8a936519250b40832f1c6bbc33

--- device-calyx.mk
+++ device-calyx.mk
@@ -9,3 +9,7 @@ DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay-calyx
 # For Google Camera
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/sysconfig/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
+
+# HBM
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/permissions/permissions_com.android.hbmsvmanager.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.hbmsvmanager.xml
