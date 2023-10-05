CalyxOS/device_google_bonito

device-calyx: Set TARGET_PREBUILT_KERNEL
Change-Id: Ifbc8e8dcaae2689f76d58ec1ed4c4aeec9794b34

device-calyx.mk
@@ -13,3 +13,5 @@ PRODUCT_COPY_FILES += \
 # For Google Camera
 PRODUCT_COPY_FILES += \
     device/google/bonito/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
+
+TARGET_PREBUILT_KERNEL := device/google/bonito-kernel/Image.lz4

CalyxOS/device_google_tangorpro

Change-Id: I156a4af77bd619f855f734983b24677531156c4d

device-calyx.mk
@@ -3,3 +3,5 @@ DEVICE_PACKAGE_OVERLAYS += device/google/tangorpro/overlay-calyx
 # For Google Camera
 PRODUCT_COPY_FILES += \
     device/google/tangorpro/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
+
+TARGET_PREBUILT_KERNEL := device/google/tangorpro-kernel/Image.lz4
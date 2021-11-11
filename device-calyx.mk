1e0785bab07c97c9ab0312b99afad968fa908ab8
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-12 04:48:12 +0530

    device-calyx: Set TARGET_PREBUILT_KERNEL
    Change-Id: I156a4af77bd619f855f734983b24677531156c4d

--- device-calyx.mk
+++ device-calyx.mk
@@ -3,3 +3,5 @@ DEVICE_PACKAGE_OVERLAYS += device/google/tangorpro/overlay-calyx
 # For Google Camera
 PRODUCT_COPY_FILES += \
     device/google/tangorpro/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
+
+TARGET_PREBUILT_KERNEL := device/google/tangorpro-kernel/Image.lz4
