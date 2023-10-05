936e8cced3ff70860f612e1f1e36e137391b9811
Michael Bestas <mkbestas@gmail.com>
2023-10-05 13:29:54 +0300

    Update blobs for 14
    Change-Id: I21ecd7e3c304986e3aa24b8874d742d0a1596c6d

--- device-calyx.mk
+++ device-calyx.mk
@@ -31,7 +31,7 @@ PRODUCT_PACKAGES += \
     android.hardware.media.c2@1.0.vendor \
     libacryl \
     libacryl_hdr_plugin \
-    libavservices_minijail.vendor \
+    libavservices_minijail.vendor:64 \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
     libexynosutils \
@@ -43,9 +43,16 @@ PRODUCT_PACKAGES += \
 
 # Confirmation UI
 PRODUCT_PACKAGES += \
-    android.hardware.confirmationui@1.0-lib.trusty:64 \
     android.hardware.confirmationui@1.0.vendor:64 \
+    android.hardware.confirmationui-V1-ndk.vendor:64 \
+    android.hardware.confirmationui-lib.trusty:64 \
     libteeui_hal_support.vendor:64
+
+# Graphics
+PRODUCT_PACKAGES += \
+    libEGL_angle \
+    libGLESv1_CM_angle \
+    libGLESv2_angle
 
 # HIDL
 PRODUCT_PACKAGES += \
@@ -54,6 +61,7 @@ PRODUCT_PACKAGES += \
 
 # Identity credential
 PRODUCT_PACKAGES += \
+    android.hardware.identity-V5-ndk.vendor:64 \
     android.hardware.identity-support-lib.vendor:64 \
     android.hardware.identity_credential.xml
 
@@ -66,6 +74,7 @@ PRODUCT_PACKAGES += \
     libnos_client_citadel:64 \
     libnos_datagram:64 \
     libnos_datagram_citadel:64 \
+    libnos_feature:64 \
     libnos_transport:64 \
     nos_app_avb:64 \
     nos_app_identity:64 \
@@ -82,32 +91,33 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge \
     sensors.dynamic_sensor_hal
 
-# Wi-Fi
+# Trusty
 PRODUCT_PACKAGES += \
-    libwifi-hal:64
+    android.trusty.stats.nw.setter-cpp.vendor:64 \
+    libbinder_trusty \
+    libtrusty_metrics:64
 
 # Misc interfaces
 PRODUCT_PACKAGES += \
+    android.frameworks.stats-V1-cpp.vendor:64 \
     android.frameworks.stats-V1-ndk.vendor:32 \
-    android.hardware.authsecret@1.0.vendor:64 \
-    android.hardware.biometrics.common-V2-ndk.vendor:64 \
-    android.hardware.biometrics.fingerprint-V2-ndk.vendor:64 \
+    android.hardware.authsecret-V1-ndk.vendor:64 \
+    android.hardware.biometrics.common-V3-ndk.vendor:64 \
+    android.hardware.biometrics.fingerprint-V3-ndk.vendor:64 \
+    android.hardware.health-V1-ndk.vendor \
     android.hardware.input.common-V1-ndk.vendor:64 \
     android.hardware.input.processor-V1-ndk.vendor:64 \
+    android.hardware.keymaster-V4-ndk.vendor:64 \
     android.hardware.keymaster@3.0.vendor:64 \
     android.hardware.keymaster@4.0.vendor:64 \
     android.hardware.keymaster@4.1.vendor:64 \
     android.hardware.neuralnetworks-V4-ndk.vendor:64 \
-    android.hardware.oemlock@1.0.vendor:64 \
+    android.hardware.oemlock-V1-ndk.vendor:64 \
     android.hardware.thermal@1.0.vendor:32 \
     android.hardware.thermal@2.0.vendor:32 \
-    android.hardware.weaver@1.0.vendor:64 \
-    android.hardware.wifi@1.1.vendor:64 \
-    android.hardware.wifi@1.2.vendor:64 \
-    android.hardware.wifi@1.3.vendor:64 \
-    android.hardware.wifi@1.4.vendor:64 \
-    android.hardware.wifi@1.5.vendor:64 \
-    android.hardware.wifi@1.6.vendor:64 \
+    android.hardware.thermal-V1-ndk.vendor:32 \
+    android.hardware.weaver-V2-ndk.vendor:64 \
+    android.hardware.wifi-V1-ndk.vendor:64 \
     com.google.hardware.pixel.display-V6-ndk.vendor
 
 # Properties
