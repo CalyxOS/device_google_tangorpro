76e04867c16a806f6a5cb4bc5d27c81d71a93e85
Michael Bestas <mkbestas@gmail.com>
2024-10-25 21:55:33 +0300

    Adapt to new build prop override scheme again
    Change-Id: I82d30c9d35506eec2891296eba0a4a6bf727aeff

--- calyx_tangorpro.mk
+++ calyx_tangorpro.mk
@@ -22,7 +22,7 @@ PRODUCT_MODEL := Pixel Tablet
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc=tangorpro-user[[:space:]]15[[:space:]]AP3A.241005.015[[:space:]]12366759[[:space:]]release-keys \
+    BuildDesc="tangorpro-user 15 AP3A.241005.015 12366759 release-keys" \
     BuildFingerprint=google/tangorpro/tangorpro:15/AP3A.241005.015/12366759:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
