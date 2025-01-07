ced6edbebaa054bd778bba74820d00fe6014a317
Chirayu Desai <chirayudesai1@gmail.com>
2025-01-08 04:26:24 +0530

    Update fingerprint/build description from AP4A.250105.002
    Change-Id: I76f36447e53b4f3f13e149b398e75e04ababc0a8

--- calyx_tangorpro.mk
+++ calyx_tangorpro.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := Pixel Tablet
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc="tangorpro-user 15 AP4A.241205.013 12621605 release-keys" \
-    BuildFingerprint=google/tangorpro/tangorpro:15/AP4A.241205.013/12621605:user/release-keys \
+    BuildDesc="tangorpro-user 15 AP4A.250105.002 12701944 release-keys" \
+    BuildFingerprint=google/tangorpro/tangorpro:15/AP4A.250105.002/12701944:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
