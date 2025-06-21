2a81c64b5682fcbc59ea0f95d003cd78934870e4
Michael Bestas <mkbestas@gmail.com>
2025-06-21 10:45:41 +0300

    tangorpro: Switch to our own kernel
    Change-Id: Iaec4d3704b2b44cd809f24d922f6965312da71e5

--- device-tangorpro.mk
+++ device-tangorpro.mk
@@ -17,9 +17,8 @@
 TARGET_RECOVERY_DEFAULT_ROTATION := ROTATION_LEFT
 
 TARGET_LINUX_KERNEL_VERSION := $(RELEASE_KERNEL_TANGORPRO_VERSION)
-# Keeps flexibility for kasan and ufs builds
-TARGET_KERNEL_DIR ?= $(RELEASE_KERNEL_TANGORPRO_DIR)
-TARGET_BOARD_KERNEL_HEADERS ?= $(RELEASE_KERNEL_TANGORPRO_DIR)/kernel-headers
+TARGET_KERNEL_DIR := device/google/tangorpro-kernels/calyx
+TARGET_BOARD_KERNEL_HEADERS := $(TARGET_KERNEL_DIR)/kernel-headers
 
 BOARD_WITHOUT_RADIO := true
 
@@ -74,14 +73,6 @@ PRODUCT_COPY_FILES += \
 # Recovery files
 PRODUCT_COPY_FILES += \
         device/google/tangorpro/conf/init.recovery.device.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.tangorpro.rc
-
-# insmod files. Kernel 5.10 prebuilts don't provide these yet, so provide our
-# own copy if they're not in the prebuilts.
-# TODO(b/369686096): drop this when 5.10 is gone.
-ifeq ($(wildcard $(TARGET_KERNEL_DIR)/init.insmod.*.cfg),)
-PRODUCT_COPY_FILES += \
-	device/google/tangorpro/init.insmod.tangorpro.cfg:$(TARGET_COPY_OUT_VENDOR_DLKM)/etc/init.insmod.tangorpro.cfg
-endif
 
 # Camera
 PRODUCT_COPY_FILES += \
