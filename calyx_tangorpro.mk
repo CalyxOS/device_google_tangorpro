773cacceaebeedc13a57b5803f88a19548f4c78e
Michael Bestas <mkbestas@gmail.com>
2025-08-05 21:17:29 +0000

    Update fingerprint/build description from BP2A.250805.005
    Change-Id: Ica86fbdab3a02b053f56c52bf05f75699789f029

--- calyx_tangorpro.mk
+++ calyx_tangorpro.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := Pixel Tablet
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc="tangorpro-user 16 BP2A.250605.031.A2 13578606 release-keys" \
-    BuildFingerprint=google/tangorpro/tangorpro:16/BP2A.250605.031.A2/13578606:user/release-keys \
+    BuildDesc="tangorpro-user 16 BP2A.250805.005 13691446 release-keys" \
+    BuildFingerprint=google/tangorpro/tangorpro:16/BP2A.250805.005/13691446:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
